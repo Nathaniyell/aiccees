3e43c46dc5bf1a831dae08b3097696070085e9f9
Nathaniyell <essien.nathan@yahoo.com>
2024-11-01 11:34:31 +0100

    update PresentConference.tsx

--- src/components/sections/PresentConference.tsx
+++ src/components/sections/PresentConference.tsx
@@ -95,7 +95,7 @@ export default function PresentConference() {
     <section className="w-full py-12 md:py-24 bg-green-50">
       <div className="container md:w-11/12 mx-auto px-4 md:px-6">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-6 text-green-600">
-          2024 AICCEES Conference
+          AICCEES 2024
         </h2>
         <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl/relaxed text-center mb-6">
           Get a glimpse of the exciting moments from AICCEES 2024 so far.
