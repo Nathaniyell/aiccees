e8900e3d4973cf61fd7546d7d974dc79e2ec93fc
Nathaniyell <essien.nathan@yahoo.com>
2025-01-03 09:18:19 +0100

    remove description from paper submission section

--- src/components/sections/PublishingPapers.tsx
+++ src/components/sections/PublishingPapers.tsx
@@ -7,11 +7,11 @@ const PublishingPapers = () => {
   return (
     <AOS>
     <section id="publish" className="w-full py-12 md:py-24  bg-[#fafafa]">
-          <div className="container md:w-11/12 mx-auto px-4 md:px-6">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-4 text-green-600">Publishing Papers</h2>
-            <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-center mb-6">
+          <div className="container md:w-11/12 mx-auto px-4 md:px-6 mb-10">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-4 text-green-600">Submitting Papers</h2>
+            {/* <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-center mb-6">
               Share your research with the energy experts, students and researchers across Africa. Submit your papers for a chance to present at the yearly Africa International Conference on Energy and Environmental Studies (AICCEES)
-            </p>
+            </p> */}
             <div className="flex justify-center gap-4">
               <Button className="bg-green-800 text-white hover:bg-green-700">Register for conference</Button>
               <Button className="bg-red-600 text-white hover:bg-red-700">Submit Your Paper</Button>
