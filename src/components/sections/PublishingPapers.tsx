c4be90e9a11600f4781d2f7860b7a0905f6f2172
Nathaniyell <essien.nathan@yahoo.com>
2025-01-12 20:59:42 +0100

    add aos to button section

--- src/components/sections/PublishingPapers.tsx
+++ src/components/sections/PublishingPapers.tsx
@@ -16,6 +16,7 @@ const PublishingPapers = () => {
 
         </div>
         <AnimatedFlowchart />
+        <AOS>
         <div className="flex flex-col sm:flex-row justify-center gap-4 px-4 md:px-0">
           <Button asChild className="bg-green-800 text-white hover:bg-green-700">
             <Link target="_blank" href="https://docs.google.com/forms/d/1FxgRduMD96dFgxJW6RtaZgCOUD2rfnfPp_D6U4_kAoA/edit">Register for conference</Link>
@@ -28,6 +29,7 @@ const PublishingPapers = () => {
             Submit Your Paper
           </Link>
         </div>
+        </AOS>
       </section>
     </AOS>
   )
