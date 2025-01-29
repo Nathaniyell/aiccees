2539766a91735227245361391cc72e8d7eafb720
Nathaniyell <essien.nathan@yahoo.com>
2025-01-29 10:55:39 +0100

    update WhatsApp widget with specific contact details

--- src/components/WhatsappLive.tsx
+++ src/components/WhatsappLive.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image'
 
 const WhatsappLive = () => {
     const [isVisible, setIsVisible] = useState(false);
+    const whatsappNumber = '+2347035151088'
+    const whatsappMessage = 'Dear Dr. Veronica, I want to enquire about AICCEES 2025'
 
     useEffect(() => {
         const handleScroll = () => {
@@ -20,7 +22,7 @@ const WhatsappLive = () => {
 
     return (
         <div className="relative">
-            <a href="https://wa.me/6281234567890" target="_blank" rel="noopener noreferrer"
+            <a href={`https://api.whatsapp.com/send/?phone=${whatsappNumber}&text=${whatsappMessage}&type=phone_number&app_absent=0`} target="_blank" rel="noopener noreferrer"
                 className="fixed bottom-20 left-4 z-[9999]"
             >
                 <span className='absolute left-0 top-0 size-16 animate-pulse'>
