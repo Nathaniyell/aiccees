0d4f5fbe3c68f9c9eed16e27fd9c9174f23dbda1
Nathaniyell <essien.nathan@yahoo.com>
2025-01-29 10:39:13 +0100

    show the icon when user has scrolled

--- src/components/WhatsappLive.tsx
+++ src/components/WhatsappLive.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import whatsappIcon from '@/public/images/whatsapp.png'
 import Image from 'next/image'
 
 const WhatsappLive = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrolled = window.scrollY;
+            setIsVisible(scrolled > 150);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    if (!isVisible) return null;
+
     return (
         <div>
             <a href="https://wa.me/6281234567890" target="_blank" rel="noopener noreferrer"
-                className="fixed bottom-8 left-4 z-50"
+                className="fixed bottom-16 left-4 z-50"
             >
-                <span className='absolute left-[7px] top-[7px] size-10 -z-50'>
-                    <span className='flex size-full items-center justify-center bg-green-500 rounded-full opacity-75'>
-
-                        <Image src={whatsappIcon} alt="Whatsapp" className="w-16 h-16 z-50" />
+                <span className='absolute left-0 top-0 size-16 -z-50 animate-pulse'>
+                    <span className='flex size-full items-center justify-center bg-green-500 rounded-full opacity-75 shadow-lg'>
+                        <Image
+                            src={whatsappIcon}
+                            alt="Whatsapp"
+                            className="z-50 size-14 rounded-full p-2 hover:scale-110 transition-transform duration-300"
+                        />
                     </span>
-
                 </span>
             </a>
         </div>
