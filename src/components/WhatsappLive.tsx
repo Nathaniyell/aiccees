31a7862d7a6413cbaa19a193b2303fb637f6029d
Nathaniyell <essien.nathan@yahoo.com>
2025-01-29 10:49:44 +0100

    update z-index for whatsapp widget

--- src/components/WhatsappLive.tsx
+++ src/components/WhatsappLive.tsx
@@ -19,16 +19,16 @@ const WhatsappLive = () => {
     if (!isVisible) return null;
 
     return (
-        <div>
+        <div className="relative">
             <a href="https://wa.me/6281234567890" target="_blank" rel="noopener noreferrer"
-                className="fixed bottom-16 left-4 z-50"
+                className="fixed bottom-20 left-4 z-[9999]"
             >
-                <span className='absolute left-0 top-0 size-16 -z-50 animate-pulse'>
+                <span className='absolute left-0 top-0 size-16 animate-pulse'>
                     <span className='flex size-full items-center justify-center bg-green-500 rounded-full opacity-75 shadow-lg'>
                         <Image
                             src={whatsappIcon}
                             alt="Whatsapp"
-                            className="z-50 size-14 rounded-full p-2 hover:scale-110 transition-transform duration-300"
+                            className="size-14 rounded-full p-2 hover:scale-110 transition-transform duration-300"
                         />
                     </span>
                 </span>
