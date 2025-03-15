64b5d7a7cf758b26b45f22d81d6be1a4f652a0ef
Nathaniyell <essien.nathan@yahoo.com>
2025-03-15 22:34:04 +0100

    Remove cardfooter import

--- src/components/sections/exhibitions.tsx
+++ src/components/sections/exhibitions.tsx
@@ -3,7 +3,7 @@ import { Check } from "lucide-react"
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function Exhibitions() {
   return (
