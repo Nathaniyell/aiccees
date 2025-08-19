18067345135400a2186145c9d2ab5aa93d474aec
Nathaniel Essien <thaniessy@gmail.com>
2025-08-19 22:12:10 +0100

    Add Keynote Speakers components for modular structure and improved organization

---
+++ src/components/keynote-speakers/index.ts
@@ -0,0 +1,6 @@
+export { default as KeynoteSpeakersHero } from './KeynoteSpeakersHero';
+export { default as RegistrationCTA } from './RegistrationCTA';
+export { default as SpeakersHeader } from './SpeakersHeader';
+export { default as SpeakersTabs } from './SpeakersTabs';
+export { default as SpeakerCard } from './SpeakerCard';
+export { default as CallToAction } from './CallToAction';
